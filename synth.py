192493b9b52a4ddc0f3034473f32bbac7a5e42cb
Alex Wang <ice_b0und@hotmail.com>
2019-11-21 12:00:08 +0800

    [feature] Add compress/limiter and sampler

--- synth.py
+++ synth.py
@@ -1,9 +1,11 @@
 #%matplotlib notebook
 #import IPython.display as ipd
 
+from datetime import datetime
 import numpy as np
 import scipy.signal
 import math
+import sys
 from toposort import toposort, toposort_flatten
 
 
@@ -12,6 +14,9 @@ import librosa.display
 import sounddevice as sd
 
 
+# just for debug purpose
+np.set_printoptions(threshold=sys.maxsize)
+np.seterr(all='raise')
 
 #### helpers
 # pan in (-60, 60)
@@ -55,6 +60,21 @@ def noise(A, pan):
         n = t.shape[-1]
         y = np.random.random(n + a)
         return panning(y[:n-a] + 1j * y[a:], pan)
+    return _f
+
+def sampler(A, file_path):
+    rate, x = scipy.io.wavfile.read(file_path)
+    x = x.T
+    if x.dtype == np.int16:
+        x = x.astype(float) / 2**15
+    
+    def _f(sr, f, t):
+        assert(rate==sr)
+        n = t.shape[-1]
+        if n > x.shape[-1]:
+            return np.append(x, np.zeros((2, n - x.shape[-1])))
+        else:
+            return x[:, :n]
     return _f
 
 
@@ -140,10 +160,95 @@ def ssb_modulator(f_c, carrier_func=np.cos):
         return carrier_func(2*np.pi * f_c/sr * np.arange(n)) * x.real - \
              np.sign(f_c) * carrier_func(2*np.pi * f_c/sr * np.arange(n) + np.pi/2) * x.imag
     return _f
-        
+
+#### Dynamic Range Control
+
+
+# DAFX: p110
+def limiter(threshold_db, attack_time, release_time, delay_time, plot=False):
+    def _f(sr, x):
+        threshold = 10 ** (threshold_db/10)
+        at = 1 - math.exp(-2.2/(attack_time*sr))
+        rt = 1 - math.exp(-2.2/(release_time*sr))
+        n = x.shape[-1]
+        delay_n = round(delay_time*sr)
+        def calculate(x_in):
+            xpeak = np.array([0, 0])
+            gain = np.array([1, 1])
+            y = np.full_like(x_in, 0)
+            abs_xn = np.abs(x_in)
+            gains = np.full_like(x_in, 0)
+            for i in range(n):
+                k = np.where(abs_xn[:, i] > xpeak, at, rt)
+                xpeak = (1-k)*xpeak + k*abs_xn[:, i]
+                # Do not replace this with min(1, threshold/xpeak) for DivisionByZero error
+                f = np.full_like(xpeak, 0)
+                for j in range(len(xpeak)):
+                    f[j] = threshold/xpeak[j] if xpeak[j] > threshold else 1
+                k = np.where(f < gain, at, rt)
+                gain = (1-k)*gain + k*f
+                gains[:, i] = gain
+                y[:, i] = gain * x_in[:, i-delay_n] if i-delay_n >= 0 else 0
+            return y, gains
+        y_real, gain_real = calculate(x.real)
+        y_imag, _ = calculate(x.imag)
+        if plot:
+            plt.plot(np.arange(x.shape[-1])/sr, 10*np.log10(gain_real[0, :]))
+        return y_real + 1j*y_imag
+    return _f
+
+
+# DAFX: p112
+def compressor(compressor_threshold_db, 
+               compressor_scale, 
+               expander_threshold_db, 
+               expander_scale, 
+               attack_time, 
+               release_time, 
+               delay_time, 
+               average_time, 
+               plot=False):
+    def _f(sr, x):
+        at = 1 - math.exp(-2.2/(attack_time*sr))
+        rt = 1 - math.exp(-2.2/(release_time*sr))
+        tav = 1 - math.exp(-2.2/(average_time*sr))
+        n = x.shape[-1]
+        delay_n = round(delay_time*sr)
+        def calculate(x_in):
+            xrms = np.array([0, 0])
+            gain = np.array([1, 1])
+            y = np.full_like(x_in, 0)
+            gains = np.full_like(x_in, 0)
+            for i in range(n):
+                xrms = (1-tav)*xrms + tav*x_in[:, i]*x_in[:, i]
+                gdb = np.full_like(xrms, 0)
+                for j in range(len(xrms)):
+                    if xrms[j] == 0:
+                        gdb[j] = 0
+                    else:
+                        xdb = 10 * np.log10(xrms[j])
+                        #print('xdb', xdb)
+                        gdb[j] = min(
+                            0, 
+                            compressor_scale*(compressor_threshold_db-xdb), 
+                            expander_scale*(expander_threshold_db-xdb))
+                f = 10**(gdb/20)
+                k = np.where(f < gain, at, rt)
+                gain = (1-k)*gain + k*f
+                
+                gains[:, i] = gain
+                y[:, i] = gain * x_in[:, i-delay_n] if i-delay_n >= 0 else 0
+            return y, gains
+        y_real, gain_real = calculate(x.real)
+        y_imag, _ = calculate(x.imag)
+        if plot:
+            plt.plot(np.arange(x.shape[-1])/sr, 10*np.log10(gain_real[0, :]))
+        return y_real + 1j*y_imag
+    return _f
+
 
 #### A simple player and mixer
-def mix(sr, freq, time_points, generators, filters, connections, output_channels=('0',)):
+def mix(sr, freq, time_points, generators, filters, connections, output_channels=('0',), profile=True):
     deps = {}
     for f, t in connections:
         if t in deps:
@@ -154,21 +259,47 @@ def mix(sr, freq, time_points, generators, filters, connections, output_channels
     channel_outs = {}
     
     sort_result = toposort(deps)
+    
+    profile_generator = {}
+    
+    processed_channels = set()
+    
+    all_channels = set([x for x in generators] + [x for x in filters])
+
+    
+    def process_own_channel(channel, channel_in=np.zeros([2, len(time_points)], dtype='complex128')):
+        channel_out = channel_in
+        if channel in generators:
+            for i, gen in enumerate(generators[channel]):
+                t1 = datetime.now()
+                channel_out += gen(sr, freq, time_points)
+                t2 = datetime.now()
+                if profile:
+                    print('channel "%s", id=%d, generator "%s", time=%s' % (channel, i, gen, t2-t1))
+
+        # If not filters, assume passing through
+        if channel in filters:
+            for i, filt in enumerate(filters[channel]):
+                t1 = datetime.now()
+                channel_out = filt(sr, channel_out)
+                t2 = datetime.now()
+                if profile:
+                    print('channel "%s", id=%d, filter "%s", time=%s' % (channel, i, filt, t2-t1))
+                    
+        return channel_out
+    
     for channels in sort_result:
         for channel in channels:
-            channel_out = np.zeros([2, len(time_points)], dtype='complex128')
+            channel_in = np.zeros([2, len(time_points)], dtype='complex128')
             if channel in deps:
                 for dep_channel in deps[channel]:
-                    channel_out += channel_outs[dep_channel]
-            if channel in generators:
-                for gen in generators[channel]:
-                    channel_out += gen(sr, freq, time_points)
-            # If not filters, assume passing through
-            if channel in filters:
-                for filt in filters[channel]:
-                    channel_out = filt(sr, channel_out)
-            
-            channel_outs[channel] = channel_out
+                    channel_in += channel_outs[dep_channel]
+            channel_outs[channel] = process_own_channel(channel, channel_in)
+            processed_channels.add(channel)
+
+    for channel in all_channels - processed_channels:
+        channel_outs[channel] = process_own_channel(channel)
+
     ret = []
     for c in output_channels:
         ret.append(channel_outs[c])
@@ -178,7 +309,7 @@ def mix(sr, freq, time_points, generators, filters, connections, output_channels
 def plot_dft(sr, y, title='', ylim=None):
     z = np.fft.fft(y)
     mag = np.abs(np.real(z)) / (len(y)/2)
-    db = np.log10(mag) * 10
+    db = np.log10(np.where(mag > 1e-10, mag, 1e-10)) * 10
     #phi = np.angle(z) / np.pi * 180
     
     fs = np.fft.fftfreq(y.shape[-1]) * sr
@@ -224,7 +355,7 @@ def easy_visualize(sr, y):
     plt.show()
 
 sr = 44100
-T = 4
+T = 2
 t = np.linspace(0, T, int(T*sr))
 f = 220
 
@@ -236,6 +367,12 @@ generators = {
     'sine': [
         sine(A=0.5, pan=-30),
     ],
+    'drums': [
+        sampler(A=0.5, file_path='drums.wav'),
+    ],
+    'piano': [
+        sampler(A=0.5, file_path='piano.wav'),
+    ]
 }
 
 filters = {
@@ -264,6 +401,17 @@ filters = {
     ],
     'ssb': [
         ssb_modulator(f_c=-2)
+    ],
+    'compressor': [
+        compressor(compressor_threshold_db=-40,
+                   compressor_scale=0.9,
+                   expander_threshold_db=0,
+                   expander_scale=1,
+                   attack_time=0.01, 
+                   release_time=0.01, 
+                   delay_time=0.001,
+                   average_time=0.05,
+                   plot=True)
     ]
 }
 connections = [
@@ -277,9 +425,10 @@ connections = [
     ('saw', 'pm'),
     ('saw', 'fm'),
     ('saw', 'ssb'),
+    ('piano', 'compressor'),
 ]
 
-y_complex, = mix(sr, f, t, generators, filters, connections, output_channels=('ssb',))
+y_complex, = mix(sr, f, t, generators, filters, connections, output_channels=('compressor',))
 y = y_complex.real
 
 # scipy wants y to be (nsamples, nchannels)
@@ -295,4 +444,5 @@ scipy.io.wavfile.write('audio.wav', sr, y.T.astype('float32'))
 
 # When in ipython play sound in this way
 #ipd.Audio(y, rate=sr)
+
 
